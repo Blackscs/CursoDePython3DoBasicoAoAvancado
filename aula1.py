af8857317c41cbc469b124064d7c168ea1b58b2e
Blackscs <marioosantos238412@gmail.com>
2023-09-09 23:15:22 -0300

    aula 1 e 2

---
+++ aula1.py
@@ -0,0 +1,13 @@
+"""
+DocString
+
+ aqui dá para escrever os seus comentários em várias linhas 
+ 1 - a
+ 2 - b 
+ 3 - c
+"""
+
+# Permite escrever um comentário
+print(123) # Na frente 
+# Abaixo
+print(123)
---
+++ aula2.py
@@ -0,0 +1,12 @@
+# \r\n = formas de quebrar a linha
+
+print(12, 34, sep="-") 
+print(56, 78, sep='-') 
+
+"""
+print() = é uma função que exibe o argumento que é inserido nela 
+argumento = é uma coisa que você passa para funções ou métodos 
+
+'12, 34, 56 e 78' são argumentos não nomeados 
+o sep é um argumento nomeado
+"""
